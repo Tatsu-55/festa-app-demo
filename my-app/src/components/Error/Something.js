567f02500c6833b5e650b96552c6ab59ea3f3103
山田達就 <yamada.tatsunari.r3@dc.tohoku.ac.jp>
2023-08-21 14:17:10 +0900

    a

--- my-app/src/components/Error/Something.js
+++ my-app/src/components/Error/Something.js
@@ -1,7 +1,7 @@
 const Something = () => {
     return (
         <>
-        <p>This is something<br></br>You will be able to vote tomorrow.</p>
+        <p>一日に投票できる回数は一回です。<br></br>明日以降、再度投票することができます。同じアイドルに投票することも可能です。</p>
         </>
     )
 }
