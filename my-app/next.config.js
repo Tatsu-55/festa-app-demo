4f61810c42022868a84be6c9351a8daf2c1e6601
山田達就 <yamada.tatsunari.r3@dc.tohoku.ac.jp>
2023-08-21 13:45:54 +0900

    commit

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['57012872b0d8c21e607fb36431b70655.ipfscdn.io'],
+  
+  },
 }
-
 module.exports = nextConfig
